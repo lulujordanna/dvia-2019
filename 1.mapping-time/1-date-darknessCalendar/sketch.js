26fed734b2b67fe36f87994c2fff15ebde423aaf
Lulu Tanenbaum <lulutanenbaum@lulus-macbook.wireless.newschool.edu>
2019-09-25 17:56:52 -0400

    Updates to process README

--- 1.mapping-time/1-date-darknessCalendar/sketch.js
+++ 1.mapping-time/1-date-darknessCalendar/sketch.js
@@ -15,27 +15,39 @@ function draw() {
   var summerColour = 200;
   var fallColour = 50;
 
-  let currentMonth = now.progress.month*0; 
+  let currentMonth = now.month; 
 
   /*January*/
   push(); 
   fill(winterColour);
   stroke(234, 234, 234);
   rect(x, y, 115, rectHeight); 
+  if (currentMonth === 1) {
+    stroke(69, 122, 191);
+    rect(x, y, 115, rectHeight); 
+  }
   pop(); 
 
   /*February*/
   push();
   fill(winterColour); 
   stroke(234, 234, 234);
-  rect(x+115, y, 100, rectHeight);  
+  rect(x+115, y, 100, rectHeight); 
+  if (currentMonth === 2) {
+    stroke(69, 122, 191);
+    rect(x+115, y, 100, rectHeight); 
+  }
   pop(); 
 
   /*March*/
   push();
   fill(springColour);
   stroke(234, 234, 234); 
-  rect(x+215, y, 85, rectHeight);  
+  rect(x+215, y, 85, rectHeight); 
+  if (currentMonth === 3) {
+    stroke(69, 122, 191);
+    rect(x+215, y, 85, rectHeight); 
+  }
   pop(); 
 
   /*April*/
@@ -43,6 +55,10 @@ function draw() {
   fill(springColour); 
   stroke(234, 234, 234);
   rect(x+300, y, 85, rectHeight); 
+  if (currentMonth === 4) {
+    stroke(69, 122, 191);
+    rect(x+300, y, 85, rectHeight);
+  }
   pop(); 
 
   /*May*/
@@ -50,6 +66,10 @@ function draw() {
   fill(springColour);
   stroke(234, 234, 234); 
   rect(x, y+100, 75, rectHeight); 
+  if (currentMonth === 5) {
+    stroke(69, 122, 191);
+    rect(x, y+100, 75, rectHeight);
+  }
   pop(); 
 
   /*June*/
@@ -57,6 +77,10 @@ function draw() {
   fill(summerColour); 
   stroke(234, 234, 234);
   rect(x+75, y+100, 55, rectHeight); 
+  if (currentMonth === 6) {
+    stroke(69, 122, 191);
+    rect(x+75, y+100, 55, rectHeight); 
+  } 
   pop(); 
 
   /*July*/
@@ -64,20 +88,31 @@ function draw() {
   fill(summerColour); 
   stroke(234, 234, 234);
   rect(x+130, y+100, 55, rectHeight); 
+  if (currentMonth === 7) {
+    stroke(69, 122, 191);
+    rect(x+130, y+100, 55, rectHeight);
+  } 
   pop(); 
 
   /*August*/
   push(); 
   fill(summerColour); 
   stroke(234, 234, 234);
-  rect(x+185, y+100, 55, rectHeight); 
+  rect(x+185, y+100, 55, rectHeight);
+  if (currentMonth === 8) {
+    stroke(69, 122, 191);
+    rect(x+185, y+100, 55, rectHeight);
+  } 
   pop(); 
 
   /*September*/
   push(); 
   fill(fallColour); 
-  stroke(currentMonth);
   rect(x+240, y+100, 75, rectHeight); 
+  if (currentMonth === 9) {
+    stroke(69, 122, 191);
+    rect(x+240, y+100, 75, rectHeight); 
+  }
   pop(); 
 
   /*October*/
@@ -85,19 +120,31 @@ function draw() {
   fill(fallColour);
   stroke(234, 234, 234); 
   rect(x+315, y+100, 70, rectHeight); 
+  if (currentMonth === 10) {
+    stroke(69, 122, 191);
+    rect(x+315, y+100, 70, rectHeight); 
+  }
   pop(); 
 
   /*November*/
   push(); 
   fill(fallColour); 
   stroke(234, 234, 234);
-  rect(x, y+200, 115, rectHeight); 
+  rect(x, y+200, 115, rectHeight);
+  if (currentMonth === 11) {
+    stroke(69, 122, 191);
+    rect(x, y+200, 115, rectHeight); 
+  } 
   pop(); 
 
   /*December*/
   push(); 
   fill(winterColour); 
   stroke(234, 234, 234);
-  rect(x+115, y+200, 125, rectHeight); 
+  rect(x+115, y+200, 125, rectHeight);
+  if (currentMonth === 12) {
+    stroke(69, 122, 191);
+    rect(x+115, y+200, 125, rectHeight); 
+  }  
   pop(); 
 }
